c875c7165f764c0e11fc7828a858cebbe90a26d4
Wilmer Rodriguez <wilmerr09@gmail.com>
2023-09-11 22:34:39 -0300

    First commit

---
+++ Seccion05-TareasPorHacerConsola/app.js
@@ -0,0 +1,61 @@
+const { guardarDB, leerDB } = require('./helpers/guardarArchivo')
+const { inquirerMenu,
+        pausa,
+        leerInput, 
+        listadoTareasBorrar,
+        confirmar } = require('./helpers/inquirer')
+const Tarea = require('./models/tarea')
+const Tareas = require('./models/tareas')
+require('colors')
+
+
+
+console.clear()
+
+
+const main = async() => {
+
+  let opt = ''
+  const tareas = new Tareas()
+
+  const tareasDB = leerDB()
+
+  if(tareasDB) {
+    tareas.cargarTareasFromArray(tareasDB)
+  }
+
+  do {
+    opt = await inquirerMenu()
+    
+    switch (opt) {
+      case '1':
+        const desc = await leerInput('Descripción:')
+        tareas.crearTarea(desc)
+        break;
+      case '2':
+        tareas.listadoCompleto()
+        break;
+      case '3':
+        tareas.listarPendientesCompletadas(true)
+        break;
+      case '4':
+        tareas.listarPendientesCompletadas(false)
+        break;
+      case '6':
+        const id = await listadoTareasBorrar(tareas.listadoArr)
+        const confirmarBorrar = confirmar('¿Estas seguro?')
+        if(confirmarBorrar) {
+          tareas.borrarTarea( id )
+        }
+        console.log('Tarea Borrada!')
+        break;
+    }
+
+    guardarDB(tareas.listadoArr)
+
+    await pausa()
+  } while (opt !== '0')
+}
+
+
+main()
