554cf6aa84c91a02446f800071aa78c76d34eb2f
Wilmer Rodriguez <wilmerr09@gmail.com>
2023-09-14 15:40:31 -0300

    Fin Seccion 6 curso-nodejs

--- Seccion06-AplicacionDeClima/index.js
+++ Seccion06-AplicacionDeClima/index.js
@@ -1,7 +1,5 @@
-const { leerInput, inquirerMenu, pausa } = require("./helpers/inquirer")
+const { leerInput, inquirerMenu, pausa, listarLugares } = require("./helpers/inquirer")
 const Busquedas = require("./models/busquedas")
-
-
 
 const main = async () => {
 
@@ -14,20 +12,32 @@ const main = async () => {
     switch (opt) {
       case 1:
 
-        const lugar = await leerInput('Ciudad: ')
-        console.log(lugar)
-      
-      console.log('\nInformación de la ciudad\n'.green)
-      console.log('Ciudad: '.green)
-      console.log('Latitud: '.green)
-      console.log('Longitud: '.green)
-      console.log('Temperatura: '.green)
-      console.log('Mínima: '.green)
-      console.log('Máxima: '.green)
+        const termino = await leerInput('Ciudad: ')
+        const lugares = await busquedas.ciudad( termino )
+        const idLugar = await listarLugares(lugares)
+        if(idLugar === '0') continue;
+
+        const lugarSelec = lugares.find(l => l.id === idLugar)
+        busquedas.agregarHistorial(lugarSelec.nombre)
+        
+        const clima = await busquedas.climaLugar(lugarSelec.lat, lugarSelec.lng)
+
+        console.clear()
+        console.log('\nInformación de la ciudad\n'.green)
+        console.log('Ciudad: ', lugarSelec.nombre.green)
+        console.log('Latitud: ', lugarSelec.lat)
+        console.log('Longitud: ', lugarSelec.lng)
+        console.log('Temperatura: ', clima.temp)
+        console.log('Mínima: ' , clima.min)
+        console.log('Máxima: ', clima.max)
+        console.log('Como está el clima: ', clima.desc.green)
         
         break;
       case 2:
-        
+        busquedas.historialCapitalizado.forEach((lugar, i) => {
+          const idx = `${i + 1}.`.green
+          console.log(`${idx} ${lugar} `)
+        })
         break;
       case 3:
         
