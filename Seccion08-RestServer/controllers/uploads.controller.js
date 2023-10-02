53780ee2631be5ed1ba0208792f071b317f5be0a
Wilmer Rodriguez <wilmerr09@gmail.com>
2023-10-02 16:35:44 -0300

    finish seccion 13

--- Seccion08-RestServer/controllers/uploads.controller.js
+++ Seccion08-RestServer/controllers/uploads.controller.js
@@ -1,9 +1,13 @@
-
+const path = require('path')
+const fs = require('fs');
 const { request, response } = require("express");
 const { uploadFile } = require("../helpers");
 const { User, Product } = require("../models");
+const cloudinary = require('cloudinary').v2
 
 
+
+cloudinary.config(process.env.CLOUDINARY_URL)
 
 const uploadArchive = async (req = request, res = response) => {
 
@@ -52,6 +56,15 @@ const updateImage = async (req, res = response) => {
       break;
   }
 
+  
+
+  if (model.image) {
+    const pathImage = path.join(__dirname, '../uploads', collection, model.image)
+    if (fs.existsSync(pathImage)) {
+      fs.unlinkSync(pathImage)
+    }
+  }
+
   const nameFile = await uploadFile(req.files, undefined, collection);
   model.image = nameFile 
 
@@ -61,7 +74,110 @@ const updateImage = async (req, res = response) => {
 }
 
 
+
+const updateImageCloudinary = async (req, res = response) => {
+  
+  const {id, collection} = req.params;
+
+  let model;
+
+  switch (collection) {
+    case 'users':
+      model = await User.findById(id)
+      if (!model) {
+        return res.status(400).json({
+          message: 'Not exists user ' + id
+        })
+      }
+      break;
+
+    case 'products':
+      model = await Product.findById(id)
+      if (!model) {
+        return res.status(400).json({
+          message: 'Not exists product ' + id
+        })
+      }
+      break;
+  
+    default:
+      res.status(500).json({message: 'Error update image'})
+      break;
+  }
+
+  
+
+  if (model.image) {
+    const nameArr = model.image.split('/');
+    const name = nameArr[nameArr.length - 1];
+    const folder = nameArr[nameArr.length - 2];
+    const [ public_id ] = name.split('.');
+    const path = folder + '/' + public_id
+    cloudinary.uploader.destroy(path);
+  }
+
+  const { tempFilePath } = req.files.myFile
+  const { secure_url } = await cloudinary.uploader.upload(tempFilePath, {
+    folder: "Curso-NodeJs-FernandoHerrera"
+  })
+
+  model.image = secure_url 
+  await model.save()
+
+  res.json(model)
+}
+
+
+
+
+
+
+
+const getImage = async (req, res = response) => {
+  
+  const { id, collection } = req.params;
+
+  let model;
+
+  switch (collection) {
+    case 'users':
+      model = await User.findById(id)
+      if (!model) {
+        return res.status(400).json({
+          message: 'Not exists user ' + id
+        })
+      }
+      break;
+
+    case 'products':
+      model = await Product.findById(id)
+      if (!model) {
+        return res.status(400).json({
+          message: 'Not exists product ' + id
+        })
+      }
+      break;
+  
+    default:
+      res.status(500).json({message: 'Error get image'})
+      break;
+  }
+
+  
+
+  if (model.image) {
+    const pathImage = path.join(__dirname, '../uploads', collection, model.image)
+    if (fs.existsSync(pathImage)) {
+      return res.sendFile(pathImage)
+    }
+  }
+  const pathImage = path.join(__dirname, '../assets/no-image.jpg');
+  res.sendFile(pathImage);
+}
+
 module.exports = {
   uploadArchive,
-  updateImage
+  updateImage,
+  getImage,
+  updateImageCloudinary
 }
--- Seccion08-RestServer/routes/uploads.routes.js
+++ Seccion08-RestServer/routes/uploads.routes.js
@@ -1,10 +1,18 @@
 const { Router } = require('express')
 const { check } = require('express-validator')
-const { uploadArchive, updateImage } = require('../controllers/uploads.controller')
+const { uploadArchive, getImage, updateImageCloudinary } = require('../controllers/uploads.controller')
 const { validateInputs, validateFile } = require('../middlewares')
 const { allowedCollections } = require('../helpers')
 
 const router = Router()
+
+router.get('/:collection/:id', [
+  check('id', 'Not valid ID').isMongoId(),
+  check('collection').custom(c => allowedCollections(c, ['users', 'products'])),
+  validateInputs
+],
+getImage
+)
 
 router.post('/', validateFile, uploadArchive)
 
@@ -13,7 +21,7 @@ router.put('/:collection/:id',[
   check('id', 'Not valid ID').isMongoId(),
   check('collection').custom(c => allowedCollections(c, ['users', 'products'])),
   validateInputs
-], updateImage)
+], updateImageCloudinary)
 
 
 
